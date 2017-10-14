8575c9bd15cab146d6cd487e00aebdbfdded11bd
michael <campbemg@uw.edu>
2017-10-13 17:22:48 -0700

    make utils pep8 compliant

--- src/utils.py
+++ src/utils.py
@@ -6,12 +6,14 @@ import numpy as np
 from bs4 import BeautifulSoup
 import multiprocessing
 
+
 def parrallel_map(function, iterable, n_jobs=8):
     pool = multiprocessing.pool.Pool(n_jobs)
     result = pool.map(function, iterable)
     pool.close()
     pool.join()
     return result
+
 
 def _HTMLEntitiesToUnicode(text):
     """Converts HTML entities to unicode.  For example '&amp;' becomes
@@ -89,7 +91,7 @@ def tokenize(tweet):
     return words
 
 
-def _encode_char(char,embedding_dim=126):
+def _encode_char(char, embedding_dim=126):
     """encodes a character as a 1 hot vector of
     length 128.
 
@@ -111,6 +113,7 @@ def _encode_char(char,embedding_dim=126):
         temp_array[0] = 1
     return temp_array
 
+
 def _encode_tweet(tweet, embedding_dim=126, max_sequence_length=140):
     """Encodes a tweet as a series of character vectors
     and pads the tweet if it is shorter than 140 characters
@@ -125,13 +128,13 @@ def _encode_tweet(tweet, embedding_dim=126, max_sequence_length=140):
     nparray of character vectors, shape = [140,128]
     """
     def get_char(i):
-        return tweet[i] if i<len(tweet) else ''
+        return tweet[i] if i < len(tweet) else ''
     padded = np.vstack([_encode_char(get_char(i)) for i in range(
         max_sequence_length)])
     return padded
 
 
-def _encode_tweet_collection(tweets,max_sequence_length, embedding_dim):
+def _encode_tweet_collection(tweets, max_sequence_length, embedding_dim):
     """takes a collection of tweets and converts them into
     an array of shape [n_tweets,n_characters,n_encoding]
 
@@ -147,9 +150,8 @@ def _encode_tweet_collection(tweets,max_sequence_length, embedding_dim):
     lengths:
         array of tweet lengths, shape=[n_tweets]
     """
-    #encodings = np.stack([self._encode_tweet(tweet) for tweet in tweets])
+    # encodings = np.stack([self._encode_tweet(tweet) for tweet in tweets])
     encodings = parrallel_map(_encode_tweet, tweets, n_jobs=3)
 
     lengths = [min(len(tweet), max_sequence_length) for tweet in tweets]
     return encodings, np.array(lengths)
-
